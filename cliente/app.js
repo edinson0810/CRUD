6433f7f52978db8f8e083556b5d488f147defdf6
edinson0810 <lache081015@gmail.com>
2025-03-21 16:50:33 -0500

    completada clase del sabado

--- cliente/app.js
+++ cliente/app.js
@@ -1,50 +1,57 @@
 import listar from "./app/ciudades/listar.js";
-import { tipo } from "./app/ciudades/listar.js";
+import llenadoDocumento from "./app/ciudades/listarDocumento.js";
+import insertar from "./app/usuarios/crear.js";
 
+const ciudad = document.querySelector("#ciudad");
+const ciudades = await listar();
+const documentos = await llenadoDocumento();
 
-
-const ciudad = document.querySelector("#ciudades");
-
- const ciudades = await listar();
-
- const llenadoCiudad = (ciudades, elemento) => {
+// listar ciudades
+const llenadoCiudad = (ciudades,elemento) => {
     const fragmento = document.createDocumentFragment();
     const option = document.createElement("option");
-    option.textContent = "seleccione ciudad";
-    fragmento.append(option);
-ciudades.forEach(({id, nombre}) => {
-     const option = document.createElement("option");
-    option.textContent = nombre;
-    option.value = id;
+    option.textContent = "Seleccione ciudad...";
     fragmento.append(option)
-      
-});
+    ciudades.forEach(({id, nombre}) => {
+        const option = document.createElement("option");
+        option.textContent = nombre;
+        option.value = id;
+        fragmento.append(option);
+    });
+    elemento.append(fragmento);
+}
 
-
-elemento.append(fragmento);
- }
-const llenadoDocumento = (documentos, elemento) => {
+// listar documentos
+const llenadoDocumentos = (documentos, elemento)=>{
     const fragmento = document.createDocumentFragment();
     const option = document.createElement("option");
-    option.textContent = "seleccione...";
+    option.textContent = "Seleccione documento...";
     fragmento.append(option);
-    documentos.forEach(({id, nombre}) => {
-        const option =document.createElement("option");
+    documentos.forEach(({id, nombre}) =>{
+        const option = document.createElement("option");
         option.textContent = nombre;
         option.value = id;
         fragmento.append(option);
     })
-    elemento.append(fragmento);
+    elemento.append(option);
 }
 
-
- 
-const guardarUsuario = () =>{
+const btn = document.querySelector('button[type="submit"]');
+const guardarUsuario = () => {
     const data = {
-
+        nombre: nombre.value,
+        apellidos: apellidos.value,
+        ciudad: ciudad.selectedIndex,
+        telefono: telefono.value,
+        direccion: direccion.value,
+        tipo_documento: tipo_documento.selectedIndex,
+        documento : documento.value,
+        email: email.value
     }
-insertar(data)
+    insertar(data)
 }
 
-  llenadoCiudad(ciudades, ciudad);
+llenadoCiudad(ciudades, ciudad);
+llenadoDocumentos(documentos, documento);
 
+btn.addEventListener("submit", guardarUsuario);
